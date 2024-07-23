aa1e6ca08a7fab0611d1690fdabde3954531bc55
Anurag-Bhattacharya4199 <anuragbtor@gmail.com>
2024-07-23 09:27:39 -0400

    Made changes to make application work as per instructions

--- force-app/main/default/lwc/accountFinder/accountFinder.js
+++ force-app/main/default/lwc/accountFinder/accountFinder.js
@@ -1,24 +1,10 @@
-import { LightningElement, track, wire } from 'lwc';
-import queryAccountsByRevenue from '@salesforce/apex/AccountListControllerLwc.queryAccountsByRevenue';
-
+import { LightningElement } from 'lwc';
 export default class AccountFinder extends LightningElement {
-    @track annualRevenue = null;
-
+    annualRevenue = null;
     handleChange(event) {
-        this.annualRevenue = event.target.value;
+        this.annualRevenue = event.detail.value;
     }
-
     reset() {
         this.annualRevenue = null;
-    }
-
-    @wire(queryAccountsByRevenue, { annualRevenue: '$annualRevenue' })
-    wiredAccounts({ error, data }) {
-        if (data) {
-            this.accounts = data;
-        } else if (error) {
-            this.accounts = undefined;
-            // Handle error
-        }
     }
 }
